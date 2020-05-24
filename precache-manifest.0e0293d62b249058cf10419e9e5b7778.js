4e98e056af6216aa79c8a1066c7415be1e7cea10
sangeetsub <sangeetsub@gmail.com>
2020-05-24 11:57:33 -0500

    Updates

---
+++ precache-manifest.0e0293d62b249058cf10419e9e5b7778.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e02ea12cf0264b95b6c4297bfc8d1d25",
+    "url": "/pwa/index.html"
+  },
+  {
+    "revision": "ef99a40e0b55f066fdb4",
+    "url": "/pwa/static/css/main.7bcfea3a.chunk.css"
+  },
+  {
+    "revision": "adbd1032a08e0a71b692",
+    "url": "/pwa/static/js/2.fae4c510.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pwa/static/js/2.fae4c510.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef99a40e0b55f066fdb4",
+    "url": "/pwa/static/js/main.60f47d3e.chunk.js"
+  },
+  {
+    "revision": "0493e0e69925e2278114",
+    "url": "/pwa/static/js/runtime-main.bc3405b4.js"
+  }
+]);
